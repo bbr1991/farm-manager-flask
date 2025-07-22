e4f052fbd69b909ee31daf562a352008bc53bd52
Bashari Abdullahi Musa <kangalbabura@gmail.com>
2025-07-22 09:54:44 -0700

    Test removing external CDN dependencies

--- static/sw.js
+++ static/sw.js
@@ -1,48 +1,15 @@
-// Define a new cache name to force a complete update
-const CACHE_NAME = 'farm-manager-cache-v6-simple-test';
+// This is the simplest possible service worker.
+// It does absolutely nothing. Its only purpose is to register successfully.
 
-// A minimal list of files to cache. We will only cache the root and main CSS/JS
-const URLS_TO_CACHE = [
-  '/',
-  '/static/css/style.css',
-  '/static/js/main.js'
-];
-
-// Standard install event - no changes here
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('SIMPLE TEST: Caching app shell');
-        return cache.addAll(URLS_TO_CACHE);
-      })
-  );
+self.addEventListener('install', (event) => {
+  console.log('V7 Simple Install: Success!');
 });
 
-// Standard activate event - no changes here
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener('activate', (event) => {
+  console.log('V7 Simple Activate: Success!');
 });
 
-// SIMPLEST POSSIBLE FETCH HANDLER
-// This is the "Cache First" strategy for ALL requests.
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // If we find a match in the cache, return it.
-        // If not, try to fetch it from the network.
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener('fetch', (event) => {
+  // Do nothing. Just go to the network.
+  return;
 });
